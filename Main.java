baf5aedd6371d320c2ec9b1daf0a9595c31f8107
Ebrahim Nouri <37481802+EbrahimNouri@users.noreply.github.com>
2026-05-01 17:22:41 +0330

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Telegram channel reader...");
         
-        String channelUsername = "proxymtproto"; // without @
+        // String channelUsername = "proxymtproto"; // without @
         String channelUsername = "mitivpn"; // without @
         String logFile = "posts_log.txt";
         
