667bd543d669a4f3b42db024420c53d3d7b75313
Ebrahim Nouri <37481802+EbrahimNouri@users.noreply.github.com>
2026-05-02 19:07:14 +0330

    Create PostData.java

---
+++ src/PostData.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class PostData {
+    String folderName;
+    String postId;
+    String dateStr;
+    String text;
+    List<String> files = new ArrayList<>();
+    boolean hasPhoto;
+    boolean hasVideo;
+    boolean hasDocument;
+}
